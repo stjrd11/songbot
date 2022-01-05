22ee9bdc77e6a56f80cc966dae88dd7063f8fcb5
stjrd11 <75235760+stjrd11@users.noreply.github.com>
2022-01-05 17:47:45 -0500

    Update csvmethods.py
    Added more functions to help support main bot.py

--- csvmethods.py
+++ csvmethods.py
@@ -1,8 +1,14 @@
 import pandas as pd
+from datetime import datetime
+import random
 
 def createBackup():
-    df = pd.read_csv('data.csv')
-    df.to_csv('dataBackup.csv', index=False)
+    df1 = pd.read_csv('data.csv')
+    df1.to_csv('dataBackup.csv', index=False)
+    df2 = pd.read_csv('bannedSongs.csv')
+    df2.to_csv('bannedSongsBackup.csv', index=False)
+    df3 = pd.read_csv('savedSongs.csv')
+    df3.to_csv('savedSongsBackup.csv', index=False)
 
 def addUser(user):
     df = pd.DataFrame({'username':[user],
@@ -10,7 +16,9 @@ def addUser(user):
                         'bump': [0],
                         'bumpLimit': [0],
                         'playedLimit': [0],
-                        'extend': [0]})
+                        'extend': [0],
+                        'raffleWins': [0],
+                        'joinEligible': [0]})
     df.to_csv('data.csv', mode='a', index=False, header=False)
 
 def getUserExtended(user):
@@ -46,3 +54,73 @@ def findUser(user):
         return False
     else:
         return True
+
+def banCheck(url):
+    df = pd.read_csv('bannedSongs.csv')
+    for i in range(len(df.link)):
+        if url == df.link[i]:
+            return True
+        else:
+            return False
+
+def getDate(seconds):
+    seconds = int(seconds)
+    date_time = datetime.fromtimestamp(seconds)
+    date = date_time.strftime("%b %d, %Y")
+    return date
+
+def checkRaffle(user):
+    df = pd.read_csv('raffleList.csv')
+    for i in range(len(df.username)):
+        if user == df.username[i]:
+            return 1
+        else:
+            return 0
+
+def checkWinner(user):
+    df = pd.read_csv('data.csv')
+    for i in range(len(df.username)):
+        if user == df.username[i]:
+            if df.joinEligible[i] == 0:
+                return 0
+            else:
+                return 1
+
+def addToRaffle(user):
+    df = pd.DataFrame({'username':[user]})
+    df.to_csv('raffleList.csv', mode='a', index=False, header=False)
+
+def getWinner():
+    df = pd.read_csv('raffleList.csv')
+    rows = len(df.axes[0])
+    if rows == 0:
+        return 0
+    elif rows == 1:
+        return df.username[1]
+    else:
+        rows = rows - 1
+        randomNum = random.randint(0, rows - 1)
+        user = df.username[randomNum]
+        return user
+
+def addRaffleWin(user):
+    df = pd.read_csv('data.csv')
+    for i in range(len(df.username)):
+        if user == df.username[i]:
+            number = df.at[i, 'raffleWins']
+            number = number + 1
+            df.at[i, 'raffleWins'] = number
+            df.at[i, 'joinEligible'] = 1
+    df.to_csv('data.csv', index=False)
+
+def resetEligible():
+    df = pd.read_csv('data.csv')
+    for i in range(len(df.username)):
+        df.at[i, 'joinEligible'] = 0
+    df.to_csv('data.csv', index=False)
+
+def resetRaffle():
+    df = pd.read_csv('raffleList.csv')
+    for i in range(len(df.username)):
+        df.drop(i, axis=0, inplace=True)
+    df.to_csv('raffleList.csv', index=False)
