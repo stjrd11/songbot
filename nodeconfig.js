2dd475d04d00234b5bc5e3deac63b92870c14edc
stjrd11 <75235760+stjrd11@users.noreply.github.com>
2022-01-23 23:53:16 -0500

    Delete nodeconfig.js

--- nodeconfig.js
+++
@@ -1,5 +0,0 @@
-export const CLIENTID = clientid;
-export const CLIENTSECRET = clientsecret;
-export const CHANNEL = channel;
-export const BITTHRESHOLD = number;
-export const RAIDCOUNT = raidcount;
